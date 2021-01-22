d4fa15dc2071be94097b9945e26101bf00a98b28
SebasBar <toolt2y@gmail.com>
2021-01-22 10:02:50 +0100

    test

---
+++ src/components/accordion/accoridion.js
@@ -0,0 +1,3 @@
+import MuiAccordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
